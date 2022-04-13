21437a478310bc1ab685b892b2bfbd55256eee55
Alaa Thr <alaethr@gmail.com>
2022-04-13 04:22:57 +0300

    first commit

---
+++ apps/student-frontoffice/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Message } from '@univ-exam/api-interfaces';
+
+@Component({
+  selector: 'univ-exam-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css'],
+})
+export class AppComponent {
+  hello$ = this.http.get<Message>('/api/hello');
+  constructor(private http: HttpClient) {}
+}

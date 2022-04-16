53d34ebdc4a79e895db890d4634a06876896f4e0
Alaa Thr <alaethr@gmail.com>
2022-04-16 23:03:23 +0300

    Add header component & Css & Js files in student-frontoffice

--- apps/student-frontoffice/src/app/app.module.ts
+++ apps/student-frontoffice/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent],
+  declarations: [AppComponent, NxWelcomeComponent, HeaderComponent],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],

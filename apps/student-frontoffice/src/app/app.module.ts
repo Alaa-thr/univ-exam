e142ee42908ce6a74cdfafc31f05080f672bc739
Alaa Thr <alaethr@gmail.com>
2022-04-16 23:37:34 +0300

    Add footer component

--- apps/student-frontoffice/src/app/app.module.ts
+++ apps/student-frontoffice/src/app/app.module.ts
@@ -5,9 +5,15 @@ import { AppComponent } from './app.component';
 import { NxWelcomeComponent } from './nx-welcome.component';
 import { HttpClientModule } from '@angular/common/http';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 @NgModule({
-  declarations: [AppComponent, NxWelcomeComponent, HeaderComponent],
+  declarations: [
+    AppComponent,
+    NxWelcomeComponent,
+    HeaderComponent,
+    FooterComponent,
+  ],
   imports: [BrowserModule, HttpClientModule],
   providers: [],
   bootstrap: [AppComponent],

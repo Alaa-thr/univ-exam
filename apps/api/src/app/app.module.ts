41ac0ad3d97fd84606f558646c85dc9e84ee941a
Alaa Thr <alaethr@gmail.com>
2022-04-25 01:06:12 +0300

    Configuration of postgresql

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,10 +1,14 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { typeOrmOptions } from './config/typeorm.config';
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(typeOrmOptions),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })

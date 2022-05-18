f9908bac9a9730b2e83c6df8f7b8d8f6529c2882
Alaa Thr <alaethr@gmail.com>
2022-05-19 00:23:24 +0300

    Add Exam Preparation component "student"

---
+++ apps/student-frontoffice/src/modules/exam-preparation/exam-preparation.component.spec.ts
@@ -0,0 +1,24 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { ExamPreparationComponent } from './exam-preparation.component';
+
+describe('ExamPreparationComponent', () => {
+  let component: ExamPreparationComponent;
+  let fixture: ComponentFixture<ExamPreparationComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ExamPreparationComponent],
+    }).compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(ExamPreparationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});

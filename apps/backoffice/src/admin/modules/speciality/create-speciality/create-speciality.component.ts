44a2132b934b8ddd81951d57f254b537d936b553
Alaa Thr <alaethr@gmail.com>
2022-06-09 22:34:02 +0300

    Add Speciality component "backoffice"

---
+++ apps/backoffice/src/admin/modules/speciality/create-speciality/create-speciality.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'univ-exam-create-speciality',
+  templateUrl: './create-speciality.component.html',
+  styleUrls: ['./create-speciality.component.css'],
+})
+export class CreateSpecialityComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit(): void {}
+}

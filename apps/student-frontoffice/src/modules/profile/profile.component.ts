0451b31b5a605bc897961a8e34c13722d236b98c
Alaa Thr <alaethr@gmail.com>
2022-06-18 05:52:50 +0300

    Add Download the QR code

--- apps/student-frontoffice/src/modules/profile/profile.component.ts
+++ apps/student-frontoffice/src/modules/profile/profile.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { GetTokenService } from '../../shared/services/get-token.service';
 import { IStudent, IUser } from '@univ-exam/common';
-import { NgxQrcodeErrorCorrectionLevels } from '@techiediaries/ngx-qrcode';
+import { NgxQrcodeErrorCorrectionLevels, NgxQrcodeElementTypes } from '@techiediaries/ngx-qrcode';
 
 @Component({
   selector: 'univ-exam-profile',
@@ -13,11 +13,19 @@ export class ProfileComponent implements OnInit {
   user: IUser
   student?: IStudent
   errorCorrectionLevel: any;
+  elementType: any;
+  url: any;
   constructor(private readonly getTokenService: GetTokenService) {
     this.user = this.getTokenService.getUser();
     this.student = this.user.student;
     this.errorCorrectionLevel = NgxQrcodeErrorCorrectionLevels.MEDIUM
+    this.elementType = NgxQrcodeElementTypes.CANVAS;
   }
 
   ngOnInit(): void {}
+
+  getQrCodeAsImage(): void {
+    const canvas = document.querySelector("canvas") as HTMLCanvasElement;
+    this.url= canvas.toDataURL("image/jpeg").toString();
+    }
 }
